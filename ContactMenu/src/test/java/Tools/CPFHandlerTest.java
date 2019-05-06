4e80dfc2bf7b0f463e7e17681b747c6f35082ba0
AlmeidaAndre <almeidaandreufpr@gmail.com>
2019-05-06 17:56:02 -0300

    Added JUnit test case to Tools.CPFHandler and ContactMenu.App.

---
+++ ContactMenu/src/test/java/Tools/CPFHandlerTest.java
@@ -0,0 +1,27 @@
+package Tools;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+import Tools.CPFHandler;
+
+/**
+ * Unit tests for CPFHandler class.
+ */
+public class CPFHandlerTest{
+    /**
+     * Unit test for isValid method.
+     */
+    @Test
+    public void testIsValid1True(){
+        CPFHandler cpf = new CPFHandler("100.186.309-79");
+        assertTrue(cpf.isValid());
+    }
+    @Test
+    public void testIsValid2True(){
+        CPFHandler cpf = new CPFHandler("123.456.789-10");
+        assertTrue("CPF not valid.", cpf.isValid("100.186.309-79"));
+    }
+
+}
