3a794bfa2310cb9234e10f23fdf045731ea41a36
AlmeidaAndre <almeidaandreufpr@gmail.com>
2019-05-10 00:22:08 -0300

    Added test cases to Tools.CalendarHandler

--- ContactMenu/src/test/java/Tools/CalendarHandlerTest.java
+++ ContactMenu/src/test/java/Tools/CalendarHandlerTest.java
@@ -13,12 +13,22 @@ public class CalendarHandlerTest{
      */
     @Test
     public void testCalendarBuilder(){
-        fail();
+        CalendarHandler cHandler = new CalendarHandler(05, 2019);
+        String calendarString = cHandler.CalendarBuilder();
+        String expectedString = "Maio 2019\nDo Se Te Qu Qu Se Sa \n          1  2  3  4 \n 5  6  7  8  9 10 11 \n12 13 14 15 16 17 18 \n19 20 21 22 23 24 25 \n26 27 28 29 30 31    \n                     \n";
+        System.out.println(expectedString);
+        System.out.println(calendarString);
+
+        assertEquals(expectedString, calendarString);
+        //fail();
     }
     
     @Test
-    public void testCalendarHandler(){
-        fail();
+    public void testCalendarHandlerConstructor(){
+        CalendarHandler cHandler = new CalendarHandler(05, 2019);
+        System.out.println(cHandler);
+        //assertTrue("Exception", true);
+        //fail();
     }
 
 }
